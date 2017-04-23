3b18b7765de7e3aed700ea42e8c59516822e71e6
AlexanderOrloff <crazymusik98@gmail.com>
2017-04-24 00:17:33 +0300

    Update hw14.py

--- HWpr14_var7/hw14.py
+++ HWpr14_var7/hw14.py
@@ -1,13 +1,8 @@
 import os
-def mostfiles():
-    number = {root : len(files) for root, dirs, files in os.walk('.')}
-    c = 0
-    folder = ''
-    for root in number:
-        if number[root] > c:
-            c = number[root]
-            folder = root
-    print('Количество файлов в папке по адресу', folder, ':', c)
-
-mostfiles()
-    
+def walking():
+    d = {root : len(files) for root, dirs, files in os.walk('.')}
+    maxi = max(d.values())
+    for key in d:
+        if d[key] == maxi:
+            print ('there are',maxi,'files in',key)
+walking()
